1a2c5d8e89f582b95f914ba910a161f7c9f97858
Matu VirtualMac <matu@Matus-Mac.local>
2019-05-05 05:50:03 -0700

    Puzzle15iOS hw2

---
+++ Puzzle15iOS/Puzzle15iOS/CollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.swift
+//  Puzzle15iOS
+//
+//  Created by Matu VirtualMac on 05/05/2019.
+//  Copyright © 2019 Matu VirtualMac. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+}
